07d19aeac088a2067928b2c3287e62b347da7b0c
stuncs69 <0346436@student.rocvantwente.nl>
2024-05-30 11:33:32 +0200

    betere sql handling

--- postMessage.php
+++ postMessage.php
@@ -14,28 +14,35 @@ if ($conn->connect_error) {
 
 $sql_output = "";
 
-if (isset($_POST["m"]) && $_POST["m"] != "") {
-    $message = $_POST['m'];
-    $query = "INSERT INTO `messages` (`id`, `message`, `posted`, `hidden`) VALUES (NULL, '$message', current_timestamp(), '0')";
+try {
+    if (isset($_POST["m"]) && $_POST["m"] != "") {
+        $message = $_POST['m']; // Vulnerable to SQL injection
+        $query = "INSERT INTO `messages` (`message`) VALUES ('$message');";
+        echo $query;
 
-    if ($conn->query($query) === TRUE) {
-        $last_id = $conn->insert_id;
-        $result_query = "SELECT * FROM messages WHERE id = $last_id";
-        $result = $conn->query($result_query);
-        if ($result->num_rows > 0) {
-            $inserted_message = $result->fetch_assoc();
-            $sql_output = "ID: " . $inserted_message['id'] . ", Message: " . $inserted_message['message'] . ", Posted: " . $inserted_message['posted'] . ", Hidden: " . $inserted_message['hidden'];
+        if ($conn->query($query) === TRUE) {
+            $last_id = $conn->insert_id;
+            $result_query = "SELECT * FROM messages WHERE id = $last_id";
+            $result = $conn->query($result_query);
+            if ($result->num_rows > 0) {
+                $inserted_message = $result->fetch_assoc();
+                $sql_output = "ID: " . $inserted_message['id'] . ", Message: " . $inserted_message['message'] . ", Posted: " . $inserted_message['posted'] . ", Hidden: " . $inserted_message['hidden'];
+            } else {
+                $sql_output = "Error retrieving message.";
+            }
         } else {
-            $sql_output = "Error retrieving message.";
+            $sql_output = "Error inserting message: " . $conn->error;
         }
     } else {
-        $sql_output = "Error inserting message: " . $conn->error;
-    }
-} else {
-    $sql_output = "No message was provided.";
+        $sql_output = "No message was provided.";
+    }    
+} catch (Exception $e) {
+    $sql_output = $e->getMessage();
 }
+
 
 $_SESSION['sql_output'] = $sql_output;
 header('Location: ' . $_SERVER['HTTP_REFERER']);
 
 $conn->close();
+?>
